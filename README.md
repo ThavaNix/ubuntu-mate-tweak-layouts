d34b39f8a73ec2ad930f1020623a4115b99d1a5e
ThavaNix <thavaprasanthnavaraththinam@gmail.com>
2019-08-19 19:09:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ubuntu-mate-tweak-layouts
---
+++ git.sh
@@ -0,0 +1,8 @@
+git init
+git config --global user.name "ThavaNix"
+git config --global user.email "thavaprasanthnavaraththinam@gmail.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
